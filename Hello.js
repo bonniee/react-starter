16ab482fa70afbda09a0223e696283ea7fe23552
Bonnie Eisenman <beisenman@twitter.com>
2016-03-25 18:07:12 -0400

    es6 component

--- Hello.js
+++ Hello.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-var Hello = React.createClass({
+class Hello extends React.Component {
   render() {
     return <h1>Hello, world</h1>;
   }
-});
+}
 
 export default Hello;
-
